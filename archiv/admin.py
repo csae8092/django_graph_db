db5ca4ec38471eb29c423e4817b1e1af6d6cb689
csae8092 <p.andorfer@gmail.com>
2025-09-09 08:36:08 +0200

    feat: improved admin list views

--- archiv/admin.py
+++ archiv/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 
 from archiv.models import Collection, TextSnippet
 
-admin.site.register(Collection)
-admin.site.register(TextSnippet)
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "text_snippet_count",
+    ]
+
+    def text_snippet_count(self, obj):
+        return obj.snippets.count()
+
+    text_snippet_count.short_description = "Text Snippet Count"
+
+
+@admin.register(TextSnippet)
+class TextSnippetAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "collection",
+        "text_id",
+        "content",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = ["collection"]
